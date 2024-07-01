544dc74f78da5d89e1505922150654198abb189a
david macharia <kdavidmacharia8485@gmail.com>
2024-07-01 15:59:59 +0300

    first Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import ListHellos from "./components/ListHellos"
+import Home from "./pages/Home"
+
+export default function App() {
+
+  return (
+    <>
+      <Home />
+
+      {/* <ListHellos /> */}
+    </>
+  )
+}
